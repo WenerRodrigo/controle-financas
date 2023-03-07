c0e07bfdb248651a975fa68a38082e5ecafc8a7f
Wener Rodrigo <wener19.wr@gmail.com>
2023-03-07 20:10:16 -0300

    finalizado o header

--- src/components/ResumeItem/style.js
+++ src/components/ResumeItem/style.js
@@ -32,6 +32,7 @@ export const Header = styled.header`
   justify-content: space-around;
   width: 100%;
   gap: 10px;
+  margin: 20px auto;
 
   svg {
     width: 25px;
@@ -41,5 +42,9 @@ export const Header = styled.header`
 
 export const HeaderTitle = styled.p`
     font-size: 20px;
+`
 
+export const Total = styled.span`
+    font-size: 30px;
+    font-weight: bold;
 `
